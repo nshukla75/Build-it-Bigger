f0a65842ae957c76e35c5e619b2b07b805cd6a6c
nshukla75 <nitushukla@yahoo.com>
2016-02-07 16:54:16 -0800

    Android Joke Display Library added

---
+++ jokedisplayandroidlibrary/src/main/java/com/example/nitu/jokedisplayandroidlibrary/JokeDisplayActivity.java
@@ -0,0 +1,40 @@
+package com.example.nitu.jokedisplayandroidlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class JokeDisplayActivity extends AppCompatActivity {
+    public final static String INTENT_JOKE = "INTENT_JOKE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke_display);
+        String joke = getIntent().getStringExtra(INTENT_JOKE);
+        TextView textViewJoke = (TextView) findViewById(R.id.textview_joke);
+        textViewJoke.setText(joke);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
